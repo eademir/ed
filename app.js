4b9d4fe9bcb60348e2e635cc7b17ca0d3ef68b38
root <root@localhost.localdomain>
2018-04-05 16:05:55 +0000

    serbay'a

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.creatserver( function(req, res) {
+var server = http.createServer( function(req, res) {
     fs.readFile('index.html', function(err, data) {
         res.write(data);
         res.end();
     });
 });
 
-server.listen(8080);
+server.listen(2000);
