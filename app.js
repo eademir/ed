6f41f100afbea368b7f197f66c76d87e4d659d09
root <root@localhost.localdomain>
2018-04-14 22:24:03 +0000

    güncelleme

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 //
 
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
-var server = http.createServer( function(req, res) {
-    fs.readFile('index.html', function(err, data) {
-        res.write(data);
-        res.end();
-    });
+var server = http.createServer(function (req, res) {
+
+
+    console.log(req.url);
+
+    if (req.url == '/') {
+        fs.readFile('index.html', function (err, data) {
+            res.write(data);
+            res.end('.')
+        });
+    }
+    else if (req.url == '/blog') {
+        fs.readFile('blog.html', function (err, data) {
+            res.write(data);
+        });
+    }
+    else if (req.url == '/contact') {
+        fs.readFile('contact.html', function (err, data) {
+            res.write(data);
+        });
+    }
+    else if (req.url == '/about-me') {
+        fs.readFile('about-me.html', function (err, data) {
+            res.write(data);
+        });
+    }
 });
 
-server.listen(2000);
+
+server.listen(5000);
