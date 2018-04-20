6a8c6bf75f6018e3c3235b78222946d93e8390cd
root <root@localhost.localdomain>
2018-04-20 18:03:02 +0000

    düzenltme

--- app.js
+++ app.js
@@ -6,31 +6,33 @@ var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
 
-
-    console.log(req.url);
-
     if (req.url == '/') {
         fs.readFile('index.html', function (err, data) {
             res.write(data);
-            res.end('.')
+            res.end()
         });
     }
     else if (req.url == '/blog') {
         fs.readFile('blog.html', function (err, data) {
             res.write(data);
+            res.end()
         });
     }
     else if (req.url == '/contact') {
         fs.readFile('contact.html', function (err, data) {
             res.write(data);
+            res.end()
         });
     }
     else if (req.url == '/about-me') {
         fs.readFile('about-me.html', function (err, data) {
             res.write(data);
+            res.end()
         });
     }
+
+    console.log(req.url);
 });
 
 
-server.listen(5000);
+server.listen(8000);
