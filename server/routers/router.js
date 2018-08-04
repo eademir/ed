c3968626d76d0ae3521affe2fd67bac49bcfafbf
eademir <eraydemir1@yandex.com>
2018-08-04 05:13:58 +0300

    edited as ES6

--- server/routers/router.js
+++ server/routers/router.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/controller')
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/controller')
 
 router.get('/', controller.index);
 router.get('/blog', controller.blog);
